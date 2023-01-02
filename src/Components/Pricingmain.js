c64a3d614e169baef23ef085b86fc64391a2bee5
srv077 <105067288+srv077@users.noreply.github.com>
2023-01-02 16:44:14 +0530

    price card issue

--- src/Components/Pricingmain.js
+++ src/Components/Pricingmain.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import "../Style/Pricing.css"
+
 
 export default function Pricingmain() {
   return (
--- src/Components/Productmain.js
+++ src/Components/Productmain.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../Style/Products.css"
 
 export default function Productmain() {
   return (
